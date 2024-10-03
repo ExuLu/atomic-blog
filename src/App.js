a1bfb0633daff6ef140c6747682f29f31c2c4ed9
Alena Sharai <exultantislupus@gmail.com>
2024-10-03 17:43:58 +0400

    refactor: extract Header and its children to separate files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 import { PostProvider, usePosts } from './PostContext';
+import Header from './components/Header';
 
 function createRandomPost() {
   return {
@@ -37,41 +38,6 @@ function App() {
       </PostProvider>
     </section>
   );
-}
-
-function Header() {
-  const { onClearPosts } = usePosts();
-
-  return (
-    <header>
-      <h1>
-        <span>⚛️</span>The Atomic Blog
-      </h1>
-      <div>
-        <Results />
-        <SearchPosts />
-        <button onClick={onClearPosts}>Clear posts</button>
-      </div>
-    </header>
-  );
-}
-
-function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePosts();
-
-  return (
-    <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      placeholder='Search posts...'
-    />
-  );
-}
-
-function Results() {
-  const { posts } = usePosts();
-
-  return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
 function Main() {
