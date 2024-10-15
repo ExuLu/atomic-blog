d56128ced6163429cde7d4329595bcc3fabf5b78
Alena Sharai <exultantislupus@gmail.com>
2024-10-15 21:04:11 +0400

    feat: return add post to archive

--- src/App-memo.js
+++ src/App-memo.js
@@ -65,7 +65,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive archiveOptions={archiveOptions} />
+      <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} />
       <Footer />
     </section>
   );
@@ -162,7 +162,10 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ archiveOptions: { show, title } }) {
+const Archive = memo(function Archive({
+  archiveOptions: { show, title },
+  onAddPost,
+}) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -185,7 +188,7 @@ const Archive = memo(function Archive({ archiveOptions: { show, title } }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              <button>Add as new post</button>
+              <button onClick={onAddPost}>Add as new post</button>
             </li>
           ))}
         </ul>
